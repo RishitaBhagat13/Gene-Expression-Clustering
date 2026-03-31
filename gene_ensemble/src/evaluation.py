429f3cf8c94fe6a35f1240f7ffb3cc8e5400b836
RishitaBhagat13 <btech10414.23@bitmesra.ac.in>
2026-03-31 17:40:04 +0530

    evaluation.py

--- gene_ensemble/src/evaluation.py
+++ gene_ensemble/src/evaluation.py
@@ -1,20 +1,39 @@
-from sklearn.metrics import silhouette_score, davies_bouldin_score, adjusted_rand_score
+from sklearn.metrics import (
+    silhouette_score,
+    davies_bouldin_score,
+    adjusted_rand_score
+)
 
-print("evaluation module loaded")   # debug
+print("evaluation module loaded ")
+
 
 def evaluate(X, y_true, labels, name):
     print(f"\n{name}")
-    
-    # Check if clustering is valid
+
+    # Handle DBSCAN noise (-1)
+    if -1 in labels:
+        mask = labels != -1
+        labels = labels[mask]
+        X = X[mask]
+
+        if y_true is not None:
+            y_true = y_true[mask]
+
+    # Check valid clustering
     if len(set(labels)) < 2:
-        print("Only one cluster ❌")
+        print("Only one cluster ")
         return
-    
+
     # Metrics
     sil = silhouette_score(X, labels)
-    db = davies_bouldin_score(X, labels)
-    ari = adjusted_rand_score(y_true, labels)
-    
+    dbi = davies_bouldin_score(X, labels)
+
     print("Silhouette:", round(sil, 4))
-    print("DB Index:", round(db, 4))
-    print("ARI:", round(ari, 4))
+    print("DB Index:", round(dbi, 4))
+
+    # ARI (only if true labels available)
+    if y_true is not None and len(set(y_true)) > 1:
+        ari = adjusted_rand_score(y_true, labels)
+        print("ARI:", round(ari, 4))
+    else:
+        print("ARI: Not valid (single class or missing labels)")
